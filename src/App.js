9515df3a44ab931dad99c4bb8c707f5c07ea82d3
VladimirVornikov <vornikov-vladimir@mail.ru>
2024-03-16 10:16:02 +0100

    16.03

--- src/App.js
+++ src/App.js
@@ -47,12 +47,31 @@ function App() {
 
   const handleMainButtonClick = () => {
     onSendData();
-    if (picture) {
-      const link = document.createElement('a');
-      link.href = picture;
-      link.download = 'captured_image.jpg';
-      link.click();
-    }
+    
+    const postData = {
+      picture: picture
+  };
+
+    fetch('http://localhost:8000/postData', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Response:', data);
+    })
+    .catch(error => {
+      console.error('There was a problem with your fetch operation:', error);
+    });
+
   };
 
   return (
